e776411cf590162ddb21751e1bf462f4f495d837
Ashton20271 <ashton20271@hotmail.com>
2025-09-03 20:12:27 +0800

    add players online indicator

--- main.js
+++ main.js
@@ -30,3 +30,23 @@ setupButton("btn-left", "ArrowLeft");
 setupButton("btn-right", "ArrowRight");
 setupButton("btn-up", "ArrowUp");
 setupButton("btn-down", "ArrowDown");
+
+// Add this to your JS after Firebase is initialized
+function updatePlayerCount() {
+    db.ref('playersOnline').on('value', snapshot => {
+        const count = snapshot.numChildren();
+        document.getElementById('playerCountNum').textContent = count;
+    });
+}
+
+// Register this player as online
+const playerSessionId = playerName + "_" + Math.random().toString(36).substr(2, 9);
+db.ref('playersOnline/' + playerSessionId).set(true);
+
+// Remove player from online list when leaving
+window.addEventListener('beforeunload', () => {
+    db.ref('playersOnline/' + playerSessionId).remove();
+});
+
+// Start tracking player count
+updatePlayerCount();
