746f7d2a08653ffd7411509f37c7b5320b17e563
Ashton20271 <ashton20271@hotmail.com>
2025-09-13 19:10:53 +0800

    bug fix

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ function triggerKey(code, type = "keydown") {
 function setupButton(id, key) {
   const btn = document.getElementById(id);
 
-  // Touch support
   btn.addEventListener("touchstart", e => {
     e.preventDefault();
     triggerKey(key, "keydown");
@@ -15,7 +14,6 @@ function setupButton(id, key) {
     triggerKey(key, "keyup");
   });
 
-  // Mouse support (for desktop clicks)
   btn.addEventListener("mousedown", e => {
     e.preventDefault();
     triggerKey(key, "keydown");
@@ -30,23 +28,3 @@ setupButton("btn-left", "ArrowLeft");
 setupButton("btn-right", "ArrowRight");
 setupButton("btn-up", "ArrowUp");
 setupButton("btn-down", "ArrowDown");
-
-// Add this to your JS after Firebase is initialized
-function updatePlayerCount() {
-    db.ref('playersOnline').on('value', snapshot => {
-        const count = snapshot.numChildren();
-        document.getElementById('playerCountNum').textContent = count;
-    });
-}
-
-// Register this player as online
-const playerSessionId = playerName + "_" + Math.random().toString(36).substr(2, 9);
-db.ref('playersOnline/' + playerSessionId).set(true);
-
-// Remove player from online list when leaving
-window.addEventListener('beforeunload', () => {
-    db.ref('playersOnline/' + playerSessionId).remove();
-});
-
-// Start tracking player count
-updatePlayerCount();
